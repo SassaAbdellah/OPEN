a75821e0c8fb0358fb17561a7484c6aaf1ba791d
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2012-11-25 19:44:23 +0000

    Deleted abstract class AbstractNominatimQuery, and included all relevant data inNominatimJSONPQuery (which is the only subclass by now)

--- src/OpenRideServer/joride/src/java/de/avci/joride/utils/geocoding/osm/NominatimJSONPQuery.java
+++ src/OpenRideServer/joride/src/java/de/avci/joride/utils/geocoding/osm/NominatimJSONPQuery.java
@@ -22,8 +22,174 @@ import java.io.Serializable;
  */
 @Named("nominatimq")
 @SessionScoped
-public class NominatimJSONPQuery extends AbstractNominatimQuery implements Serializable {
+public class NominatimJSONPQuery  implements Serializable {
 
+    
+    
+        /** Default value for the service_url property.
+     *  This points to the original Nominatim service at:
+     * "http://nominatim.openstreetmap.org/search"
+     */
+    protected static final String DEFAULT_SERVICE_URL = "http://nominatim.openstreetmap.org/search";
+    /** Name of the "addressdetails" parameter.
+     *  Wether to return verbose address details.
+     *  We alway chose 0 here, because we cannot process adressdetaiks anyway.
+     *  Currently, we are content with returning the displaystring,
+     *  which is always there.
+     *
+     *  See a typical Nominatim query below:
+     *  q=fliederweg&format=json&polygon=1&addressdetails=2&json_callback=callback
+     *
+     */
+    protected static final String PARAM_NAME_addressdetails = "addressdetails";
+    /** Nominatim parametername for the "format" parameter.
+     *  Format parameter defines the format of the response.
+     *  Nominatim allows 'html','xml', json,...
+     *  We alway chose json here, because technically we do
+     *  cross site scripting with jsonp (aka: padded JSON).
+     *
+     *
+     *  See a typical Nominatim query below:
+     *  q=fliederweg&format=json&polygon=1&addressdetails=2&json_callback=callback
+     *
+     */
+    protected static final String PARAM_NAME_format = "format";
+    /** Nominatim parametername for the "polygon" parameter.
+     *  Wether to return a polygon for boundary or not
+     *  We alway chose 0 here, because we cannot process polygons anyway.
+     *
+     *  See a typical Nominatim query below:
+     *  q=fliederweg&format=json&polygon=1&addressdetails=2&json_callback=callback
+     *
+     */
+    protected static final String PARAM_NAME_polygon = "polygon";
+    /** Nominatim parametername for the "querystring".
+     *  This is simply called 'q' by nominatim.
+     *
+     *  See a typical Nominatim query below:
+     *  q=fliederweg&format=json&polygon=1&addressdetails=2&json_callback=callback
+     *
+     */
+    protected static final String PARAM_NAME_q = "q";
+    /** Value of the "addressdetails" parameter.
+     *  Wether to return verbose address details.
+     *  We alway chose 0 here, because we cannot process adressdetaiks anyway.
+     *  Currently, we are content with returning the displaystring,
+     *  which is always there.
+     *
+     *  See a typical Nominatim query below:
+     *  q=fliederweg&format=json&polygon=1&addressdetails=2&json_callback=callback
+     *
+     */
+    protected static final String PARAM_VALUE_addressdetails = "0";
+    /** Value  for the "polygon" parameter.
+     *  Wether to return a polygon for boundary or not
+     *  We alway chose 0 here, because we cannot process polygons anyway.
+     *
+     *  See a typical Nominatim query below:
+     *  q=fliederweg&format=json&polygon=1&addressdetails=2&json_callback=callback
+     *
+     */
+    protected static final String PARAM_VALUE_polygon = "0";
+    /** Property telling which City to search for.
+     *  Note, that we can use either
+     *  street and number,   or  poiName.
+     *  If poiName is given, then this takes precedence over street/and number
+     *
+     */
+    protected String city = null;
+    
+      /** Trivial Getter
+     */
+    public String getCity() {
+        return this.city;
+    }
+
+
+    /** Trivial Setter
+     */
+    public  void setCity(String arg){ 
+        this.city=arg;
+    }
+    
+    
+
+    /** Property telling which POI to search for.
+     *  Note, that we can use either
+     *  street and number,   or  poiName.
+     *  If poiName is given, then this takes precedence over street/and number
+     *
+     */
+    protected String poi = null;
+    
+    /** Trivial Getter
+     */
+    public String getPoi(){ 
+        return this.poi; 
+    }
+    
+     /** Trivial Setter
+     */
+    public  void setPoi(String arg){ 
+        this.poi=arg;
+    }
+   
+    
+    
+    
+    
+    
+    /** Property telling which URL to use.
+     *  Decault is {@link DEFAULT_SERVICE_URL }
+     */
+    protected String serviceURL = NominatimJSONPQuery.DEFAULT_SERVICE_URL;
+    
+    
+    /** Trivial getter
+     */
+    public String getServiceURL(){return this.serviceURL;}
+    
+    /** Trivial Setter
+     */
+    public void setServiceURL(String arg) {this.serviceURL=arg; }
+    
+    
+    /** Property telling which Street to search for.
+     *  Note, that we can use either
+     *  street and number or  poiName.
+     *  If poiName is given, then this takes precedence over street/and number
+     *
+     */
+    protected String street = null;
+    
+       /** Trivial Getter
+     */
+    public String getStreet(){ 
+        return this.street; 
+    }
+    
+     /** Trivial Setter
+     */
+    public  void setStreet(String arg){ 
+        this.street=arg;
+    }
+
+  
+
+    
+    /** Base URL for calling the Mapper Servlet
+     * 
+     * @return 
+     */
+    public String getOSMMapperURL(){    
+        return OSMConstants.OSMMAP;
+    }
+    
+   
+    
+    
+    
+    
     /**
      * Standard Value for the "format" parameter. Format parameter defines the
      * format of the response. Nominatim allows 'html','xml', json,... We alway
