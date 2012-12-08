22658325277b4f95eb77d16f9ac5d52739adbda3
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2012-12-08 20:36:57 +0000

    fixed nullpointer exception when picking point from list of favorite points

--- src/OpenRideServer/joride/src/java/de/avci/joride/jbeans/favoritepoints/JFavoritePointEntity.java
+++ src/OpenRideServer/joride/src/java/de/avci/joride/jbeans/favoritepoints/JFavoritePointEntity.java
@@ -63,136 +63,131 @@ public class JFavoritePointEntity extends FavoritePointEntity {
 
     }
 
-    
-    
-    /** Create a new Favoritepoint for this user
-     * 
+    /**
+     * Create a new Favoritepoint for this user
+     *
      */
     public void create() {
-    
-        (new JFavoritePointsService()).addFavoritePoint(this);
-    
-    }
-    
-    
 
-    public void delete(int favpointId){
-    
+        (new JFavoritePointsService()).addFavoritePoint(this);
+
+    }
+
+    public void delete(int favpointId) {
+
         (new JFavoritePointsService()).deleteFavoritePointSavely(favpointId);
     }
-    
-    
-    
-        /** Load the FavoritePointsEntity from database with the id given
-       *  as in http request parameter.
-       *  Will do extensive checking on the backside to 
-       *  ensure that the current User (identified by http Auth principal)
-       *  has permissions to do so.
-       * 
-       */
-      public void updateFromDB(){
-                
-          int favpointId=0;
-          
-          String favpointIdStr=(new HTTPRequestUtil()).getParameterSingleValue(CRUDConstants.PARAM_NAME_CRUD_ID);
-          favpointId=(new Integer(favpointIdStr)).intValue();
-          
-          System.err.println("============ Loading Favpoint :"+favpointId+" ");
-            
-            FavoritePointEntity fpe=(new JFavoritePointsService()).getFavoritePointEntitySafely(favpointId);
-                    
-            
-            this.setFavptId(fpe.getFavptId());
-            this.setFavptDisplayname(fpe.getFavptDisplayname());
-            this.setFavptAddress(fpe.getFavptAddress());   
-            this.setFavptPoint(fpe.getFavptPoint());
-                                    
-      } // updateFromDB
-      
-      
-      
-      /** Update Point data from HTTPRequest's Parameter.
-       *  I.e: if HTTPRequest transports Address/Displaystring or Coordinate points,
-       *  then update the respective data from http request.
-       * 
-       */
-      public void smartUpdatePoints(){
-      
-          // create new point
-          WebflowPoint p=new WebflowPoint();
-          //
-          p.smartUpdate();
-          // 
-          if(p.getAddress()!=null ){
-              this.setFavptAddress(p.getAddress());
-          }
-          
-          if(p.getDisplaystring()!=null){
-              this.setFavptDisplayname(p.getDisplaystring());
-          }
-          
-          if(p.getPoint()!=null){
-              this.setFavptPoint(p.getDatabaseString());
-          }
-          
-      }
-      
-      
-      
-      /** Get Longitude from point
-       * 
-       * @return 
-       */
-      public Double getLon(){
-          
-          PostGISPointUtil pu=new PostGISPointUtil();
-          Point p=pu.pointFromDBString(this.getFavptPoint()); 
-          return pu.getLon(p);
-      }
-      
-    
-           
-      /** Get Latitude from point
-       * 
-       * @return 
-       */
-      public Double getLat(){
-          
-          PostGISPointUtil pu=new PostGISPointUtil();
-          Point p=pu.pointFromDBString(this.getFavptPoint()); 
-          return pu.getLat(p);
-      }
-      
-    
-      /** Create a new JFavoritePointEntity from a FavoritePointEntity 
-       *  (without leading "J")
-       * 
-       * @param fp 
-       */
-      
-      public JFavoritePointEntity(FavoritePointEntity fp){
-      
-          this.setCustId(fp.getCustId());
-          this.setFavptAddress(fp.getFavptAddress());
-          this.setFavptDisplayname(fp.getFavptDisplayname());
-          this.setFavptFrequency(fp.getFavptFrequency());
-          this.setFavptId(fp.getFavptId());
-          this.setFavptPoint(fp.getFavptPoint());
-               
-      }
-      
-      
-      
-      /** Dump default constructor
-       * 
-       * @param fp 
-       */
-       public JFavoritePointEntity(){    
-      }
-      
-      
-   
-      
-     
-   
+
+    /**
+     * Load the FavoritePointsEntity from database with the id given as in http
+     * request parameter. Will do extensive checking on the backside to ensure
+     * that the current User (identified by http Auth principal) has permissions
+     * to do so.
+     *
+     */
+    public void updateFromDB() {
+
+        int favpointId = 0;
+
+        String favpointIdStr = (new HTTPRequestUtil()).getParameterSingleValue(CRUDConstants.PARAM_NAME_CRUD_ID);
+        favpointId = (new Integer(favpointIdStr)).intValue();
+
+        System.err.println("============ Loading Favpoint :" + favpointId + " ");
+
+        FavoritePointEntity fpe = (new JFavoritePointsService()).getFavoritePointEntitySafely(favpointId);
+
+
+        this.setFavptId(fpe.getFavptId());
+        this.setFavptDisplayname(fpe.getFavptDisplayname());
+        this.setFavptAddress(fpe.getFavptAddress());
+        this.setFavptPoint(fpe.getFavptPoint());
+
+    } // updateFromDB
+
+    /**
+     * Update Point data from HTTPRequest's Parameter. I.e: if HTTPRequest
+     * transports Address/Displaystring or Coordinate points, then update the
+     * respective data from http request.
+     *
+     */
+    public void smartUpdatePoints() {
+
+        // create new point
+        WebflowPoint p = new WebflowPoint();
+        //
+        p.smartUpdate();
+        // 
+        if (p.getAddress() != null) {
+            this.setFavptAddress(p.getAddress());
+        }
+
+        if (p.getDisplaystring() != null) {
+            this.setFavptDisplayname(p.getDisplaystring());
+        }
+
+        if (p.getPoint() != null) {
+            this.setFavptPoint(p.getDatabaseString());
+        }
+
+    }
+
+    /**
+     * Get Longitude from point
+     *
+     * @return
+     */
+    public Double getLon() {
+
+        PostGISPointUtil pu = new PostGISPointUtil();
+        Point p = pu.pointFromDBString(this.getFavptPoint());
+
+        // protect agains null pointer exceptions
+        if (pu == null || p == null) {
+            return null;
+        }
+
+        return pu.getLon(p);
+    }
+
+    /**
+     * Get Latitude from point
+     *
+     * @return
+     */
+    public Double getLat() {
+
+        PostGISPointUtil pu = new PostGISPointUtil();
+        Point p = pu.pointFromDBString(this.getFavptPoint());
+
+        // protect agains null pointer exceptions
+        if (pu == null || p == null ) {
+            return null;
+        }
+        return pu.getLat(p);
+    }
+
+    /**
+     * Create a new JFavoritePointEntity from a FavoritePointEntity (without
+     * leading "J")
+     *
+     * @param fp
+     */
+    public JFavoritePointEntity(FavoritePointEntity fp) {
+
+        this.setCustId(fp.getCustId());
+        this.setFavptAddress(fp.getFavptAddress());
+        this.setFavptDisplayname(fp.getFavptDisplayname());
+        this.setFavptFrequency(fp.getFavptFrequency());
+        this.setFavptId(fp.getFavptId());
+        this.setFavptPoint(fp.getFavptPoint());
+
+    }
+
+    /**
+     * Dump default constructor
+     *
+     * @param fp
+     */
+    public JFavoritePointEntity() {
+    }
 } // class
