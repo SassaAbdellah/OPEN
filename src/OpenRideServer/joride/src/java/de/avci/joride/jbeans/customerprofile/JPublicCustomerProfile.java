5b44f56e5681abb59b4c2972cecfbbd29f42a12c
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2012-12-25 09:16:31 +0000

    Adding "JPublicCustomerProfile" bean to safely display subset of Personal Data towards working frontent for accepting drives.

---
+++ src/OpenRideServer/joride/src/java/de/avci/joride/jbeans/customerprofile/JPublicCustomerProfile.java
@@ -0,0 +1,83 @@
+package de.avci.joride.jbeans.customerprofile;
+
+import de.fhg.fokus.openride.customerprofile.CustomerEntity;
+import java.io.Serializable;
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/** **Public** Customer Profile.  
+ *  
+ *  This is the subset of personal data from {@link JCustomerEntity}
+ *  that may safely be displayed to other customers.
+ *  
+ *  Consequently, this gets created from JCustomerProfile
+ *  and has only Getter methods.
+ * 
+ * 
+ * 
+ *
+ * @author jochen
+ */
+
+@Named
+@RequestScoped
+
+public class JPublicCustomerProfile implements Serializable {
+    
+    
+    /** CustomerId
+     */
+    private Integer custId;
+    
+    public Integer getCustId(){
+        return this.custId;
+    }
+    
+    
+    
+    /** Customer Nickname
+     */
+    private String custNickname;
+    
+    public String getCustNickname(){
+        return this.custNickname;
+    }
+    
+    
+    /** Gender should be known to Riders a priori,
+     *  since it may effect accept decline decision
+     */
+    private char custGender;
+           
+    public char getCustGender(){
+        return custGender;
+    }
+    
+    /** Since when is this user allowed to drive.
+     *  Another important information before
+     *  deciding on whether or not to acceppt a ride offer.
+     * 
+     */
+    Date custLicensedate;
+            
+    public Date getCustLicensedate(){
+        return custLicensedate;
+    }
+    
+    
+    /** Fill with Data from givenCustomerId
+     */
+    public void updateFromCustomerEntity(CustomerEntity ce){
+   
+        this.custId=ce.getCustId();
+        this.custGender=ce.getCustGender();
+        this.custLicensedate=ce.getCustLicensedate();
+        this.custNickname=ce.getCustNickname();
+    }
+    
+    
+    
+    
+    
+} // class
