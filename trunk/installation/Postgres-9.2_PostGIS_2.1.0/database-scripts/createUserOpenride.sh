71f66c47210f4e38de0e06b7cee291516cc61c47
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2013-10-10 17:25:26 +0000

    more fixes to createUserOpenride scripts

--- trunk/installation/Postgres-9.2_PostGIS_2.1.0/database-scripts/createUserOpenride.sh
+++ trunk/installation/Postgres-9.2_PostGIS_2.1.0/database-scripts/createUserOpenride.sh
@@ -1,3 +1,3 @@
 echo "create user openride with password 'openride' " | psql
-echo "grant all privileges on openride to openride"    | psql
+echo "ALTER DATABASE openride OWNER TO    openride  " | psql
 
