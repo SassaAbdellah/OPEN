60bcf93ed54e964572081af4d822c517463586db
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2013-12-08 16:25:41 +0000

    Defining waypoint bean in the faces.config.xml fixes strange behaviour of the waypoint bean

--- src/OpenRideServer/joride/src/java/de/avci/joride/jbeans/driverundertakesride/JWaypointEntity.java
+++ src/OpenRideServer/joride/src/java/de/avci/joride/jbeans/driverundertakesride/JWaypointEntity.java
@@ -7,119 +7,132 @@ package de.avci.joride.jbeans.driverundertakesride;
 import de.avci.joride.utils.HTTPUtil;
 import de.avci.joride.utils.WebflowPoint;
 import de.fhg.fokus.openride.rides.driver.WaypointEntity;
-import de.fhg.fokus.openride.routing.RoutePoint;
-import java.util.logging.Level;
-import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
 
-/** Java Class making Waypoints accessible
- *  to frontend as a JSFBean
- * 
- *  This is session scoped, since it is supposed to model 
- *  a newly created waypoint, which live over several pages.
+/**
+ * Java Class making Waypoints accessible to frontend as a JSFBean
  *
- *  @author jochen
+ * This is session scoped, since it is supposed to model a newly created
+ * waypoint, which live over several pages.
+ *
+ * @author jochen
  */
-
-@Named("waypoint")
+@Named
 @SessionScoped
-
-/** Frontend to Waypoint Entity as a JSF Bean.
- * 
+/**
+ * Frontend to Waypoint Entity as a JSF Bean.
+ *
  */
-public class JWaypointEntity extends WaypointEntity{
-    
-    /** Parametername for rideId. Used when doing a smart update.
-     */
-    public static final String PARAM_NAME_RIDEID="rideId";
-    
+public class JWaypointEntity extends WaypointEntity implements Serializable {
+
     /**
-     * @return  parameter name for rideId Parameter
+     * Parametername for rideId. Used when doing a smart update.
      */
-    public String getParamNameRideId(){return PARAM_NAME_RIDEID;}
-    
-    /** Parametername for position. Used when doing a smart update.
+    public static final String PARAM_NAME_RIDEID = "rideId";
+
+    /**
+     * @return parameter name for rideId Parameter
      */
-    public static final String PARAM_NAME_POSITION="position";
-    
-    
+    public String getParamNameRideId() {
+        return PARAM_NAME_RIDEID;
+    }
+    /**
+     * Parametername for position. Used when doing a smart update.
+     */
+    public static final String PARAM_NAME_POSITION = "position";
+
     /**
      * @return parameter name position
      */
-    public String getParamNamePosition(){return PARAM_NAME_POSITION;}
-    
-    
+    public String getParamNamePosition() {
+        return PARAM_NAME_POSITION;
+    }
+
     /**
-     *  Clear all fields, set parameters
-     *  longitude, latitude, description,
-     *  position and rideId from parameters.
-     */
-    
-    public void smartUpdate(){
-    
-        //
-        // retrieve point coordinates and descriptions
-        // via webflow Point
-        //
-        WebflowPoint webflowPoint = new WebflowPoint();
-        webflowPoint.smartUpdate();
-        
-        this.setLongitude(webflowPoint.getLon());
-        this.setLatitude(webflowPoint.getLat());
-        this.setDescription(webflowPoint.getDisplaystring());
-        //
-        // set position parameter and rideIDs 
-        // 
-        HTTPUtil hru = new HTTPUtil();
-        String positionS = hru.getParameterSingleValue(getParamNamePosition());
-        try{ this.setPosition(new Float(positionS));
-        } catch(Exception exc){
-            exc.printStackTrace(System.err);
+     * Clear all fields, set parameters longitude, latitude, description,
+     * position and rideId from parameters.
+     */  
+    public String smartUpdate() {
+
+        try {
+
+            this.setDescription("smartUpdat has been called on this");
+            this.position = 1f;
+
+            HTTPUtil hru = new HTTPUtil();
+            String positionS = hru.getParameterSingleValue(getParamNamePosition());
+
+            try {
+                this.setPosition(new Float(positionS));
+            } catch (Exception exc) {
+                System.err.println(exc);
+            }
+
+
+            String rideIdS = hru.getParameterSingleValue(getParamNameRideId());
+            try {
+                this.setRideId(new Integer(rideIdS));
+            } catch (Exception exc) {
+                System.err.println(exc);
+            }
+
+
+            // retrieve point coordinates and descriptions
+            // via webflow Point
+
+
+            WebflowPoint webflowPoint = new WebflowPoint();
+            webflowPoint.smartUpdate();
+
+            this.setLongitude(webflowPoint.getLon());
+            this.setLatitude(webflowPoint.getLat());
+
+
+            //
+            // set position parameter and rideIDs 
+            // 
+
+        } catch (Exception exc) {
+            System.err.println(exc);
         }
-        String  rideIdS=hru.getParameterSingleValue(getParamNameRideId());
-        try{ this.setRideId(new Integer(rideIdS));
-        } catch(Exception exc){
-            exc.printStackTrace(System.err);
-        }
-        
+
+        return getJSON().toString();
     } // smartUpdate
-    
-    
-    /** JWaypoint Entity adds a volatile position parameter to WaypointEntity,
-     *  
-     *  This is used *only* for finding the routeIdx of a newly created waypoint.
-     *  
-     *  I.e a newly created waypoint "W_new" is added just before the first 
-     *  existing waypoint having routeIdx larger than w.position.
-     * 
-     * 
-     *  Position defaults to Integer.MAX_VALUE so that newly created 
-     *  waypoints without other information are added to the 
-     *  end of the ridepoints list.
-     * 
+    /**
+     * JWaypoint Entity adds a volatile position parameter to WaypointEntity,
+     *
+     * This is used *only* for finding the routeIdx of a newly created waypoint.
+     *
+     * I.e a newly created waypoint "W_new" is added just before the first
+     * existing waypoint having routeIdx larger than w.position.
+     *
+     *
+     * Position defaults to Integer.MAX_VALUE so that newly created waypoints
+     * without other information are added to the end of the ridepoints list.
+     *
      */
-    private Float position=new Float(Integer.MAX_VALUE);
-    
-    public Float getPosition(){
-        return this.getPosition();
+    private Float position = new Float(Integer.MAX_VALUE);
+
+    public Float getPosition() {
+        return this.position;
     }
-    
-    public void setPosition(float position){
-        this.position=position;
+
+    public void setPosition(float position) {
+        this.position = position;
     }
-    
-    
-    /** Encode this waypoint as a 
-     *  JSON *Array* compatible with
-     *  the representation of {@link JRoutePointEntity}
-     *  that is: [longitude,latitude,id,'description']
-     * 
-     * @return  representation of this object as a json array
-     * 
+
+    /**
+     * Encode this waypoint as a JSON *Array* compatible with the representation
+     * of {@link JRoutePointEntity} that is:
+     * [longitude,latitude,id,'description']
+     *
+     * @return representation of this object as a json array
+     *
      */
-    public StringBuffer getJSON(){
-        StringBuffer buf=new StringBuffer();
+    public StringBuffer getJSON() {
+        StringBuffer buf = new StringBuffer();
         buf.append("[");
         buf.append(this.getLongitude());
         buf.append(",");
@@ -130,17 +143,25 @@ public class JWaypointEntity extends WaypointEntity{
         buf.append("'");
         buf.append(this.getDescription());
         buf.append("'");
+        buf.append(',');
+        buf.append(this.getRideId());
+        buf.append(',');
+        buf.append(this.getPosition());
+        buf.append(',');
+        buf.append(this.getRouteIdx());
+        buf.append(',');
+        buf.append(this.hashCode());
         buf.append("]");
         return buf;
     }
-       
-    
-    /** Create Waypoint Entity from it's superclass,
-     *  copying longitude,latitude,rideId,routeIdx and description.
-     * 
+
+    /**
+     * Create Waypoint Entity from it's superclass, copying
+     * longitude,latitude,rideId,routeIdx and description.
+     *
      */
-    public JWaypointEntity(WaypointEntity  w){
-    
+    protected JWaypointEntity(WaypointEntity w) {
+
         super();
         this.setLongitude(w.getLongitude());
         this.setLatitude(w.getLatitude());
@@ -148,8 +169,12 @@ public class JWaypointEntity extends WaypointEntity{
         this.setRouteIdx(w.getRouteIdx());
         this.setDescription(w.getDescription());
     }
-    
-    
-    
-    
+
+    /**
+     * Dumb default constructor
+     */
+    public JWaypointEntity() {
+        super();
+        this.setDescription("uninitialized");
+    }
 }
