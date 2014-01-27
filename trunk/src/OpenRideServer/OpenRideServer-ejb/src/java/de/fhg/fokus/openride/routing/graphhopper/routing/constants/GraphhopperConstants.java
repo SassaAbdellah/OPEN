8027a100e85605f9e9b02228b5928d380e538f53
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2014-01-27 18:18:05 +0000

    Adding a GraphHopper RouteImplementation to openrideshare. This is to eventually replace the outdated mapsforge routing. The current delivery adds a RouteImplementation that  compiles and builds, but is currently not functionally testet at all....

---
+++ trunk/src/OpenRideServer/OpenRideServer-ejb/src/java/de/fhg/fokus/openride/routing/graphhopper/routing/constants/GraphhopperConstants.java
@@ -0,0 +1,52 @@
+package de.fhg.fokus.openride.routing.graphhopper.routing.constants;
+
+public class GraphhopperConstants {
+
+	/** Mnemonic for A-STAR-BIdirectional routing algoritm
+	 */
+	public static final String ALGORITHM_ASTARBI="astarbi";
+	/** Mnemonic for DIJKSTRA-BIdirectional routing algoritm
+	 */
+	public static final String ALGORITHM_DIJKSTRABI="dijkstrabi";
+
+
+	/** Directory where  ch graph and osm files live
+	 */
+	public static final String GRAPH_DIRECTORY = "/home/jochen/graphhopper/graphhopper-folder/";
+
+	
+	
+	
+	/** The *.osm.pbf  file to be used
+	 */
+	public static final String OSM_FILE_PATH = GRAPH_DIRECTORY
+			+ "mecklenburg-vorpommern.osm.pbf";
+	
+	
+	/** String constant that causes the EncodingManager to load a "CarFlagEncoder"
+	 */
+	public static final String CAR = "CAR";
+	
+	
+	
+	/** MaxDistance of Points parameter as used in Route.getEquiDistantRoutePoints
+	 * 
+	 *  TODO: this should be made configurable depending on the maximum 
+	 *  detour the driver is willing to make.
+	 */
+	public static final double MAX_DISTANCE_OF_POINTS=6000;
+	
+	
+	
+
+	/** wheter or not the graph should be created with ContructionHierarchies
+	 * 
+	 *  Note that for doing the routing with CH_ENABLED, the Graph must be created
+	 *  with that Option.
+	 *  
+	 * 
+	 */
+	public static boolean USE_CH=true;
+	
+	
+}
