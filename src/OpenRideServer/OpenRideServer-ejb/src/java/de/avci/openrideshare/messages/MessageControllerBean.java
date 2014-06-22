d3404c9c5e43f47a1308b511ed20c471b75580a0
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2014-06-22 14:46:27 +0000

    Basic support for abstract messages, including adding messages and sending messages

--- src/OpenRideServer/OpenRideServer-ejb/src/java/de/avci/openrideshare/messages/MessageControllerBean.java
+++ src/OpenRideServer/OpenRideServer-ejb/src/java/de/avci/openrideshare/messages/MessageControllerBean.java
@@ -8,7 +8,9 @@ import de.fhg.fokus.openride.customerprofile.CustomerEntity;
 import de.fhg.fokus.openride.helperclasses.ControllerBean;
 import de.fhg.fokus.openride.matching.MatchEntity;
 
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -16,99 +18,83 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 /**
- * Controller to finally implement a sending messages 
- * currently this is a dummy which only writes 
- * the messages to the log.
+ * Controller to finally implement a sending messages currently this is a dummy
+ * which only writes the messages to the log.
  * 
- *
+ * 
  * @author jochen
  */
 @Stateless
-public class MessageControllerBean extends ControllerBean implements MessageControllerLocal {
-	
+public class MessageControllerBean extends ControllerBean implements
+		MessageControllerLocal {
+
 	@PersistenceContext
 	private EntityManager em;
 
-    /**
-     * Message to be sent from sender to recipient if a ride request is to be
-     * cancelled
-     *
-     * Currently, this is just a dummy.
-     *
-     *
-     * @param sender
-     * @param recipient
-     * @param subject
-     * @param message
-     * @return
-     */
-    @Override
-    public boolean sendMessage(CustomerEntity sender, CustomerEntity recipient, String subject, String message) {
+	/**
+	 * Message to be sent from sender to recipient if a ride request is to be
+	 * cancelled
+	 * 
+	 * Currently, this is just a dummy.
+	 * 
+	 * 
+	 * @param sender
+	 * @param recipient
+	 * @param subject
+	 * @param message
+	 * @return
+	 */
+	@Override
+	public boolean createMessage(CustomerEntity sender, CustomerEntity recipient,
+			String subject, String message) {
 
-        Logger logger = Logger.getLogger(this.getClass().getCanonicalName());
+		Logger logger = Logger.getLogger(this.getClass().getCanonicalName());
 
-        String logstr =
-                "Sending Message:\n"
-                + "\n"
-                + "Sender: " + sender + "\n"
-                + "\n" 
-                + "Recipient: "+recipient+"\n"
-                +"\n"
-                +"Subject: "+subject+"\n"
-                +"\n"
-                +"Message: "+message;
-               
-        
-        logger.warning("Sending messages is not yet implemented : "+logstr);
-        
-            
-        return true;
-    }
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+		String logstr = "Message:\n" + "\n" + "Recipient: " + recipient + "\n"
+				+ "\n" + "Subject: " + subject + "\n" + "\n" + "Message: "
+				+ message;
 
-    @Override
-    
-    /** Currently placeholder dummy, writes the message to the logs 
-     * 
-     */
-    
-    public boolean sendSystemMessage(CustomerEntity recipient, String subject, String message) {
-        
-        
-         Logger logger = Logger.getLogger(this.getClass().getCanonicalName());
+		Message msg = new Message();
+		msg.setSender(sender);
+		msg.setRecipient(recipient);
+		msg.setMessage(message);
+		msg.setTimeStampCreated(new Date());
 
-        String logstr =
-                "Sending System Message:\n"
-                + "\n" 
-                + "Recipient: "+recipient+"\n"
-                +"\n"
-                +"Subject: "+subject+"\n"
-                +"\n"
-                +"Message: "+message;
-               
-        
-        logger.warning("Sending system messages is not yet implemented : "+logstr);
-        
+		try {
+			
+			em.persist(msg);
+			em.flush();
+			
+			logger.fine("Successfully created message : " + logstr);
+			return true;
+			
+		} catch (Exception exc) {
+			
+			logger.log(Level.SEVERE,"Error creating message : " + logstr,exc);
+			return true;
+			
+		}
+	}
 
-        return true;
-     
-    }
+	
+
+	@Override
+	/** Call sendMessage with "null" sender
+	 * 
+	 */
+	public boolean createSystemMessage(CustomerEntity recipient, String subject,
+			String message) {
+
+		return this.createMessage(null, recipient, subject, message);
+	}
 
 	@Override
 	public List<Message> findAllMessages() {
-		
+
 		Query q = em.createNamedQuery("Message.findAll");
-        List<Message> matches = q.getResultList();
+		List<Message> matches = q.getResultList();
 		return matches;
-	
+
 	}
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
