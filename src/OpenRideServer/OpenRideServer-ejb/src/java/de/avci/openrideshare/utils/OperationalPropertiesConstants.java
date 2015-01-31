f4ba6318b6002e570abd005f7b0e4de584518334
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-01-31 16:45:21 +0000

    Match limits per customer via properties, see tickets #110 and #111

---
+++ src/OpenRideServer/OpenRideServer-ejb/src/java/de/avci/openrideshare/utils/OperationalPropertiesConstants.java
@@ -0,0 +1,44 @@
+package de.avci.openrideshare.utils;
+
+
+/** Defining mnemonic property names for operational properties
+ * 
+ * @author jochen
+ *
+ */
+public class OperationalPropertiesConstants {
+	
+	
+	
+	
+	/** jndi name of the mailservice 
+	 */
+	
+	public static final String PROPERTY_NAME_mailServiceJNDI="mailServiceJNDI";
+	
+	 
+	/** email of the person/entity that gets messages 
+	 *  concerning operational issues
+	 *  (mailfunction) 
+	 */
+	public static final String PROPERTY_NAME_webmasterEmailRecipient="webmasterEmailRecipient";
+	
+	 /** email of the person/entity that gets messages
+	  * concerning business issues  (complaints etc...)
+	  * also recipient for webfrontend
+	  */
+	
+	public static final String PROPERTY_NAME_businessEmailRecipient="businessEmailRecipient";
+	
+	
+	 /** noreply email that is used as sender/replyTo address
+	  *  for messages that are not supposed to be replied
+	  */
+	public static final String PROPERTY_NAME_noreplyEmailRecipient="noreplyEmailRecipient";
+	
+	/**  max limit for matches to be displayed.
+	 */
+	public static final String PROPERTY_NAME_maxMatchLimit="maxMatchLimit";
+
+	
+}
