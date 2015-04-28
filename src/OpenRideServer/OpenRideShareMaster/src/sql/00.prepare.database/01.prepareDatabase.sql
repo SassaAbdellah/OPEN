35142afc7aa2a6f40cb53e237cf8c88150dd4890
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-04-28 04:46:27 +0000

    starting sql inventory

---
+++ src/OpenRideServer/OpenRideShareMaster/src/sql/00.prepare.database/01.prepareDatabase.sql
@@ -0,0 +1,50 @@
+--
+-- prepare potentially empty openride database for 
+-- running ORS stuff into it
+--
+--
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'SQL_ASCII';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: openride; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE openride IS 'Database for openride ridesharing app';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: postgis; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION postgis IS 'PostGIS geometry, geography, and raster spatial types and functions';
+
+
+SET search_path = public, pg_catalog;
