4b19a7695f8964ead678e8cdf7465fe55a282144
ngramm <ngramm@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-05-11 02:37:27 +0000

    Fixed the order of arguments in st_distance function

--- src/OpenRideServer/OpenRideShareMaster/src/sql/06-route-matching-modules-polar/03.orsPoldriveminimaldistance.sql
+++ src/OpenRideServer/OpenRideShareMaster/src/sql/06-route-matching-modules-polar/03.orsPoldriveminimaldistance.sql
@@ -4,13 +4,13 @@
 --  Drop before recreate
 -- -----------------------------------------------------------------------
 
-DROP FUNCTION IF EXISTS orsPoldriveminimaldistance(integer, geography); 
+DROP FUNCTION IF EXISTS orsPolDriveMinimalDistance(integer, geography); 
 
 --
--- Name: orspoldriveminimaldistance(integer, geography); Type: FUNCTION; Schema: public; Owner: openride
+-- Name: orsPolDriveMinimalDistance(integer, geography); Type: FUNCTION; Schema: public; Owner: openride
 --
 
-CREATE FUNCTION orsPoldriveminimaldistance(driveid integer, coo geography) RETURNS double precision
+CREATE FUNCTION orsPolDriveMinimalDistance(driveid integer, coo geography) RETURNS double precision
     LANGUAGE plpgsql
     AS $$
 
@@ -31,13 +31,11 @@ CREATE FUNCTION orsPoldriveminimaldistance(driveid integer, coo geography) RETUR
 
 DECLARE
 
-	 minimum double precision;
+	minimum double precision;
 	
 BEGIN
-	SELECT  into minimum min(st_distance(coo,ST_MakePoint(drp.coordinate[0],drp.coordinate[1])::geography)) from drive_route_point drp where drp.drive_id=driveId ;
+	SELECT into minimum min(ST_distance(ST_MakePoint(drp.coordinate[0],drp.coordinate[1])::geography,coo)) from drive_route_point drp where drp.drive_id=driveid ;
 	return minimum;
 END;
 
 $$;
-
-
