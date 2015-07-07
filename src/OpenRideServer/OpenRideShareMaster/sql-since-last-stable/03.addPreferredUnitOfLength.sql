5095d049d9a35c43e1b852aead32e0c54e81f9c5
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-07-07 04:43:38 +0000

    fixed update script.. preferred unit of length now gets default value 2 (kilometers)

--- src/OpenRideServer/OpenRideShareMaster/sql-since-last-stable/03.addPreferredUnitOfLength.sql
+++ src/OpenRideServer/OpenRideShareMaster/sql-since-last-stable/03.addPreferredUnitOfLength.sql
@@ -1,3 +1,4 @@
 -- Add a maximum number of matches to be displayed.
 -- this is limit may be changed by the user in a range of 1...matchLimitMax.
 ALTER TABLE customer add  preferredUnitOfLength  int;
+UPDATE customer set preferredUnitOfLength = 2  where preferredunitoflength is NULL ; 
