f7ada68b822c2e76eeef99c4021abfa22f9b8614
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-07-08 18:55:48 +0000

    Updated javascript, so google search will not return results outside of bounds. See Ticket #175

--- trunk/src/OpenRideServer/joride/web/resources/js/geocodeGoogle.js
+++ trunk/src/OpenRideServer/joride/web/resources/js/geocodeGoogle.js
@@ -1,31 +1,69 @@
-function codeAddressWithGoogle(address) {
+
+
+
+
+
+
+
+
+
+//
+// Geomapping using Google Geocoding API v3 
+//
+// https://developers.google.com/maps/documentation/javascript/?hl=de
+//
+// See reference document here
+// https://developers.google.com/maps/documentation/javascript/reference
+//
+// Note that bounds in GoogleGeocoding API are interpreted as links
+// (unlike Nominatim, which treats them as exclusive)
+// 
+// Hence, while passing the bounds to the query as a hint, we still
+// have to check every result returned if it is contained inside
+// the bounds 
+//
+//
+
+function codeAddressWithGoogle(address, north, east, south, west) {
 	
-  
+	
+	
+	
     var geocoder = new google.maps.Geocoder();
 
-	console.log("searching for address : "+address);
+	
+    // Southwest bound
+	var sw=  new google.maps.LatLng( south,  west  );
+    // northeast bound
+	var ne=  new google.maps.LatLng( north , east  );
+	// bounds object created from corners
+	var bounds= new google.maps.LatLngBounds(sw, ne);
+    
+    
+       
   	geocoder.geocode( 
-				{'address': address}, 
+				{'address': address , 'bounds' : bounds }, 
 				function(results, status) {
     					if (status == google.maps.GeocoderStatus.OK) 
 					{
     						
     				   var numberOfResults=results.length;	    
     				    // make a nice header 
-    				   $('#geosearch_out').append('\<h3\>'+geocodeResultHeader+' '+numberOfResults+'\<\/h3\>');  	
-    				
+    				 
     				   
     				   var i=0;
+    				   var count=0;
     				   
-    				   
-						while( i<results.length && i< limit ){
-							
+						while( i<results.length && count < limit ){
+						
+						// bounds are interpreted as hint only, so we have to check
+						// if result is indeed contained in bounding box
+						//	
+						if(bounds.contains( results[i].geometry.location))	{
 					        var displayName=results[i].formatted_address;
 					        var lat=results[i].geometry.location.lat();
 					        var lon=results[i].geometry.location.lng();
-							
-							
-							
+								
 					        // build the url for calling the mapper
 					        // note that early webkit browsers want to have ampersant escaped as '&aml;
 					        // which is not done by the encodeURI funtion
@@ -33,18 +71,11 @@ function codeAddressWithGoogle(address) {
 					        var mapperURL=mapURL+('\?'+lonP+'\='+lon+'&amp;'+latP+'\='+lat+'&amp;'+displayP+'\='+displayName); 
 
 					        mapperURL=encodeURI(mapperURL);
-					        
-					        // dropped as not to annoy M$ IIE
-					        // 
-					        //console.log('mapperURL : '+mapperURL);
+					      
 					        
 					           // build the URL for acceppting the place
 					        var targetedURL=targetURL+('\?'+lonP+'\='+lon+'&amp;'+latP+'\='+lat+'&amp;'+displayP+'\='+displayName+'&amp;'+targetP+'\='+target); 
 					  
-					        // dropped as not to annoy M$ IIE
-					        // 
-					        // console.log('targetedURL : '+targetedURL);
-					        
 					        
 					        // since webkit has trouble displaying divs, we had to add 
 					        // tons of ugly br tags here.
@@ -56,13 +87,20 @@ function codeAddressWithGoogle(address) {
 					        $('#geosearch_out').append(geocodeAcceptLabel.link(targetedURL));
 					        $('#geosearch_out').append('........................');
 					        $('#geosearch_out').append(geocodeShowInMapLabel.link(mapperURL));
-					        $('#geosearch_out').append('\<br\/\>')
+					        $('#geosearch_out').append('\<br\/\>');
+					        
+					        count++; // count number of results
+						}
 					        
                           i++;
 						};	
-     		 		    } else {
+     		 		    } else { // if (status == google.maps.GeocoderStatus.OK) 
       						console.log('Geocode was not successful for the following reason: ' + status);
     					}
+    					// at the end of the list, print out the number of results,
+    					// so user gets notified that there are more results
+    					$('#geosearch_out').append('\<h4\>'+geocodeResultHeader+' '+count+'\<\/h4\>');  	
+    	    				
   				  }
 				);
 	} // function codeAddress
