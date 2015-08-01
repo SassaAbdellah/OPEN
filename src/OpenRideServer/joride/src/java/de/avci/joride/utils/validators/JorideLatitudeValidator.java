f274c5eb5b269914dd126c62aace46fc48d30ace
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-08-01 11:20:10 +0000

    Validating bounds when creating drives When creating offers, startpoint and endpoint values are validated against globally defined bounds. This is related to ticket #175.

---
+++ src/OpenRideServer/joride/src/java/de/avci/joride/utils/validators/JorideLatitudeValidator.java
@@ -0,0 +1,47 @@
+package de.avci.joride.utils.validators;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.ValidatorException;
+
+import de.avci.openrideshare.boundaries.BoundariesBean;
+import de.avci.openrideshare.errorhandling.ErrorCodes;
+
+/** A simple validator checking that Points are in valid Longitude/Latitude
+ * 
+ *  Object passed to validate method is a simple double describing a longitude.
+ *  this is tested for beeing contained within valid bounds for latitude and longitude.
+ *
+ *  If point is not within valid range, it throws a generic errormessage of type "point not in bounds".
+ *
+ * @author jochen
+ *
+ */
+
+
+@FacesValidator("de.avci.joride.utils.validators.JorideLatitudeValidator")
+
+public class JorideLatitudeValidator extends JorideValidator{
+
+	
+	/** @return generic "Point exceeds spatial bounds" Errorcode
+	 */
+	protected String getErrorCode(){return ErrorCodes.SPATIAL_BOUNDS_EXCEEDED;}
+	
+	
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object latitudeO)
+			throws ValidatorException {
+		
+			Double latitude=(Double) latitudeO;
+			
+			if(! new BoundariesBean().isWithinLatitudeBounds(latitude)){
+			
+				FacesMessage message=new FacesMessage(this.getErrormessageByErrorCode(getErrorCode()));
+				throw new ValidatorException(message);
+			}
+	}
+
+}
