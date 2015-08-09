9f3373c7b73ecbed766e4930aabb83edf1b3c0b2
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-08-09 20:41:29 +0000

    Support for turning off driver/passenger/rating functionality. Just defined the properties for local.properties and integrated them with joride. No functionality yet.

--- trunk/src/OpenRideServer/joride/src/java/de/avci/joride/constants/JoRideConstants.java
+++ trunk/src/OpenRideServer/joride/src/java/de/avci/joride/constants/JoRideConstants.java
@@ -14,9 +14,10 @@ import javax.inject.Named;
 
 import de.avci.joride.utils.PropertiesLoader;
 
-/** This class centrally defines a number of Constants for joride Frontend
- *  some of these may eventually be made configurable,
- *  so it's good to have them in one spot.
+/**
+ * This class centrally defines a number of Constants for joride Frontend some
+ * of these may eventually be made configurable, so it's good to have them in
+ * one spot.
  * 
  *
  * @author jochen
@@ -24,139 +25,153 @@ import de.avci.joride.utils.PropertiesLoader;
 
 @ApplicationScoped
 @Named
+public class JoRideConstants implements Serializable {
 
-public class JoRideConstants implements Serializable{
-    
-    /**
+	/**
 	 * default serial
 	 */
 	private static final long serialVersionUID = 1L;
 
-
-    
-	
-	/**   @return  timestamp format, defined as "tsformat" property in datetime.properties
+	/**
+	 * @return timestamp format, defined as "tsformat" property in
+	 *         datetime.properties
 	 */
-	public static String getDateTimeFormatString(){
+	public static String getDateTimeFormatString() {
 		return PropertiesLoader.getDatetimeProperties().getProperty("tsformat");
 	}
-	
-	// @return  date format, defined as "dateformat" property in datetime.properties
-	public static String getDateFormatString(){
-		return PropertiesLoader.getDatetimeProperties().getProperty("dateformat");
-	}
-	
-	
-    
-    /** Creates a new DateTimeFormat 
-     *  (Datetime=Date+Time of Day)
-     *  using Timezone retrieved with "getTimezone"
-     *  
-     * @return  
-     */
-    public static DateFormat createDateFormat(){
-        DateFormat res=new SimpleDateFormat(getDateFormatString());
-        res.setTimeZone(getTimeZone());
-        return res;
-    }
-    
-    
-    
-    
-    /** Creates a new DateTimeFormat 
-     *  (Datetime=Date+Time of Day)
-     * @return  
-     */
-    public static DateFormat createDateTimeFormat(){
-       DateFormat res=new SimpleDateFormat(getDateTimeFormatString());
-       res.setTimeZone(getTimeZone());
-       return res;
-    }
-    
-    
-    /** TODO: make Timezone configurable, currently it is default timezone.
-     * 
-     * @return
-     */
-    public static TimeZone getTimeZone() {
-		return TimeZone.getTimeZone(PropertiesLoader.getDatetimeProperties().getProperty("defaulttimezone"));
+
+	// @return date format, defined as "dateformat" property in
+	// datetime.properties
+	public static String getDateFormatString() {
+		return PropertiesLoader.getDatetimeProperties().getProperty(
+				"dateformat");
 	}
 
-	/** Decimal Format for displaying ratings 
-     */
-    public DecimalFormat createRatingAverageFormat(){
-        return new DecimalFormat("#0.00");
-    }
-    
-            
-    /** JSON Parameter describing the name of the JSON object describing the update count.
-     *  This is used with JSON Update Service
-     */
-    public static String PARAM_NAME_UPDATE_RESPONSE="UpdateResponse";
-    
-    
-    /** Make  PARAM_NAME_UPDATE_RESPONSE availlable 
-     *  as a JSF Property
-     * 
-     * @return  PARAM_NAME_UPDATE_RESPONSE
-     */
-    public static String getParamNameUpdateResponse(){
-        return PARAM_NAME_UPDATE_RESPONSE;
-    }
-    
+	/**
+	 * Creates a new DateTimeFormat (Datetime=Date+Time of Day) using Timezone
+	 * retrieved with "getTimezone"
+	 * 
+	 * @return
+	 */
+	public static DateFormat createDateFormat() {
+		DateFormat res = new SimpleDateFormat(getDateFormatString());
+		res.setTimeZone(getTimeZone());
+		return res;
+	}
 
-    /** JSON Parameter describing the number of updated offers
-     *  for this user.
-     *  This is used with JSON Update Service.
-     */
-    public static String PARAM_NAME_NO_UPDATED_OFFERS="NoOfUpdatedOffers";
-    
-    
-    /** Make PARAM_NAME_NO_UPDATED_OFFERS availlable as a JSF Property.
-     * 
-     * @return  PARAM_NAME_NO_UPDATED_OFFERS
-     */
-    public String getParamNameNoUpdatedOffers(){
-        return PARAM_NAME_NO_UPDATED_OFFERS;
-    }
-    
-    
-        
-    /** JSON Parameter describing the number of updated requests for this user.
-     *  This is used with JSON Update Service
-     */
-    public static String PARAM_NAME_NO_UPDATED_REQUESTS="NoOfUpdatedRequests";
-    
-    
-    /** Make PARAM_NAME_NO_UPDATED_OFFERS availlable 
-     *  as a JSF Property
-     * 
-     * @return  PARAM_NAME_NO_UPDATED_OFFERS
-     */
-    public String getParamNameNoUpdatedRequests(){
-        return PARAM_NAME_NO_UPDATED_REQUESTS;
-    }
-    
-    
-    /** HTTP Parameter transporting Customer's Nickname
-     * 
-     */
-    protected static final String PARAM_NAME_NICKNAME="nickname";
-    
-    /** Make Nickname parameter available to JSF Beans
-     */
-    public String getParamNameNickname(){
-        return PARAM_NAME_NICKNAME;
-    }
-    
-    
-    
-    /**  "showCookieMessage" is used to decide wether or not 
-     *   cookie messages should be displayed on top of joride pages.
-     * 
-     */
-    public static final String PROPERTY_NAME_showCookieMessage="showCookieMessage";
-    
-     
-     
+	/**
+	 * Creates a new DateTimeFormat (Datetime=Date+Time of Day)
+	 * 
+	 * @return
+	 */
+	public static DateFormat createDateTimeFormat() {
+		DateFormat res = new SimpleDateFormat(getDateTimeFormatString());
+		res.setTimeZone(getTimeZone());
+		return res;
+	}
+
+	/**
+	 * TODO: make Timezone configurable, currently it is default timezone.
+	 * 
+	 * @return
+	 */
+	public static TimeZone getTimeZone() {
+		return TimeZone.getTimeZone(PropertiesLoader.getDatetimeProperties()
+				.getProperty("defaulttimezone"));
+	}
+
+	/**
+	 * Decimal Format for displaying ratings
+	 */
+	public DecimalFormat createRatingAverageFormat() {
+		return new DecimalFormat("#0.00");
+	}
+
+	/**
+	 * JSON Parameter describing the name of the JSON object describing the
+	 * update count. This is used with JSON Update Service
+	 */
+	public static String PARAM_NAME_UPDATE_RESPONSE = "UpdateResponse";
+
+	/**
+	 * Make PARAM_NAME_UPDATE_RESPONSE availlable as a JSF Property
+	 * 
+	 * @return PARAM_NAME_UPDATE_RESPONSE
+	 */
+	public static String getParamNameUpdateResponse() {
+		return PARAM_NAME_UPDATE_RESPONSE;
+	}
+
+	/**
+	 * JSON Parameter describing the number of updated offers for this user.
+	 * This is used with JSON Update Service.
+	 */
+	public static String PARAM_NAME_NO_UPDATED_OFFERS = "NoOfUpdatedOffers";
+
+	/**
+	 * Make PARAM_NAME_NO_UPDATED_OFFERS availlable as a JSF Property.
+	 * 
+	 * @return PARAM_NAME_NO_UPDATED_OFFERS
+	 */
+	public String getParamNameNoUpdatedOffers() {
+		return PARAM_NAME_NO_UPDATED_OFFERS;
+	}
+
+	/**
+	 * JSON Parameter describing the number of updated requests for this user.
+	 * This is used with JSON Update Service
+	 */
+	public static String PARAM_NAME_NO_UPDATED_REQUESTS = "NoOfUpdatedRequests";
+
+	/**
+	 * Make PARAM_NAME_NO_UPDATED_OFFERS availlable as a JSF Property
+	 * 
+	 * @return PARAM_NAME_NO_UPDATED_OFFERS
+	 */
+	public String getParamNameNoUpdatedRequests() {
+		return PARAM_NAME_NO_UPDATED_REQUESTS;
+	}
+
+	/**
+	 * HTTP Parameter transporting Customer's Nickname
+	 * 
+	 */
+	protected static final String PARAM_NAME_NICKNAME = "nickname";
+
+	/**
+	 * Make Nickname parameter available to JSF Beans
+	 */
+	public String getParamNameNickname() {
+		return PARAM_NAME_NICKNAME;
+	}
+
+	/**
+	 * "showCookieMessage" is used to decide wether or not cookie messages
+	 * should be displayed on top of joride pages.
+	 * 
+	 */
+	public static final String PROPERTY_NAME_showCookieMessage = "showCookieMessage";
+
+	
+	/***************************************************************************************/
+	/**** Frontend capability stuff comes here ! *******************************************/
+	/***************************************************************************************/
+	
+	/** Property name for driver functionality, i.e turning on/off offering rides
+	 * (true/false)
+	 */
+	public static final String PROPTERY_NAME_joride_capability_driver = "joride_capability_driver";
+
+	/** Property name for passenger functionality, i.e turning on/off requesting
+	 *  hitches (true/false)
+	 */
+
+	public static final String PROPTERY_NAME_joride_capability_passenger = "joride_capability_passenger";
+
+	/** Property name for rating functionality, i.e turning on/off rating others
+	 * (true/false)
+	 */
+
+	public static final String PROPTERY_NAME_joride_capability_rating = "joride_capability_rating";
+
 } // class
