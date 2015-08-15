19bb5f2e99a1c6d50fe27a5738116d95eefdb806
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-08-15 17:21:11 +0000

    Reverting Changes from svn:1324. See reopened ticket #177 why I did this.

--- src/OpenRideServer/joride/src/java/de/avci/joride/jbeans/driverundertakesride/JWaypointEntity.java
+++ src/OpenRideServer/joride/src/java/de/avci/joride/jbeans/driverundertakesride/JWaypointEntity.java
@@ -28,299 +28,298 @@ import javax.inject.Named;
  * Frontend to Waypoint Entity as a JSF Bean.
  *
  */
-public class JWaypointEntity extends WaypointEntity implements Serializable, Comparable<JWaypointEntity> {
+public class JWaypointEntity extends WaypointEntity implements Serializable,
+		Comparable<JWaypointEntity> {
 
-    /**
-     * Parametername for rideId. Used when doing a smart update.
-     */
-    public static final String PARAM_NAME_RIDEID = "rideId";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Parametername for rideId. Used when doing a smart update.
+	 */
+	public static final String PARAM_NAME_RIDEID = "rideId";
 
-    /**
-     * @return parameter name for rideId Parameter
-     */
-    public String getParamNameRideId() {
-        return PARAM_NAME_RIDEID;
-    }
-    /**
-     * Parametername for position. Used when doing a smart update.
-     */
-    public static final String PARAM_NAME_POSITION = "position";
+	/**
+	 * @return parameter name for rideId Parameter
+	 */
+	public String getParamNameRideId() {
+		return PARAM_NAME_RIDEID;
+	}
 
-    /**
-     * @return parameter name position
-     */
-    public String getParamNamePosition() {
-        return PARAM_NAME_POSITION;
-    }
-    /**
-     * Parametername for description. Used when doing a smart update.
-     */
-    public static final String PARAM_NAME_DESCRIPTION = "description";
+	/**
+	 * Parametername for position. Used when doing a smart update.
+	 */
+	public static final String PARAM_NAME_POSITION = "position";
 
-    /**
-     * @return parameter name position
-     */
-    public String getParamNameDescription() {
-        return PARAM_NAME_DESCRIPTION;
-    }
+	/**
+	 * @return parameter name position
+	 */
+	public String getParamNamePosition() {
+		return PARAM_NAME_POSITION;
+	}
 
-    /**
-     * Clear all fields, set parameters longitude, latitude, description,
-     * position and rideId from parameters.
-     */
-    public void smartUpdate() {
+	/**
+	 * Parametername for description. Used when doing a smart update.
+	 */
+	public static final String PARAM_NAME_DESCRIPTION = "description";
 
-        try {
-        	
-            HTTPUtil hru = new HTTPUtil();
-            String positionS = hru.getParameterSingleValue(getParamNamePosition());
+	/**
+	 * @return parameter name position
+	 */
+	public String getParamNameDescription() {
+		return PARAM_NAME_DESCRIPTION;
+	}
 
-            if (positionS != null) {
-                try {
-                    this.setPosition(new Integer(positionS));
-                } catch (Exception exc) {
-                    System.err.println(exc);
-                }
-            }
+	/**
+	 * Clear all fields, set parameters longitude, latitude, description,
+	 * position and rideId from parameters.
+	 */
+	public void smartUpdate() {
 
-            String rideIdS = hru.getParameterSingleValue(getParamNameRideId());
+		try {
 
-            if (rideIdS != null) {
-                try {
-                	Integer rideId=new Integer(rideIdS);
-                	DriverUndertakesRideEntity drive=new JDriverUndertakesRideEntityService().getDriveByIdSafely(rideId);
-                    this.setRideId(drive);
-                } catch (Exception exc) {
-                    System.err.println(exc);
-                }
-            }
+			HTTPUtil hru = new HTTPUtil();
+			String positionS = hru
+					.getParameterSingleValue(getParamNamePosition());
 
-            // retrieve point coordinates and descriptions
-            // via webflow Point
+			if (positionS != null) {
+				try {
+					this.setPosition(new Integer(positionS));
+				} catch (Exception exc) {
+					System.err.println(exc);
+				}
+			}
 
-            WebflowPoint webflowPoint = new WebflowPoint();
-            webflowPoint.smartUpdate();
+			String rideIdS = hru.getParameterSingleValue(getParamNameRideId());
 
-            this.setLongitude(webflowPoint.getLon());
-            this.setLatitude(webflowPoint.getLat());
-            
-            // if user has not set a description for this waypoint yet,
-            // description use default displaystring of chosen waypoint
-            
-            if(isNullOrEmpty(this.getDescription())){
-            	this.setDescription(webflowPoint.getDisplaystring());
-            }
-            // set position parameter and rideIDs 
+			if (rideIdS != null) {
+				try {
+					Integer rideId = new Integer(rideIdS);
+					DriverUndertakesRideEntity drive = new JDriverUndertakesRideEntityService()
+							.getDriveByIdSafely(rideId);
+					this.setRideId(drive);
+				} catch (Exception exc) {
+					System.err.println(exc);
+				}
+			}
 
-        } catch (Exception exc) {
-            System.err.println(exc);
-        }
+			// retrieve point coordinates and descriptions
+			// via webflow Point
 
+			WebflowPoint webflowPoint = new WebflowPoint();
+			webflowPoint.smartUpdate();
 
-    } // smartUpdate
-    /**
-     * JWaypoint Entity adds a volatile position parameter to WaypointEntity,
-     *
-     * This is used *only* for finding the routeIdx of a newly created waypoint.
-     *
-     * I.e a newly created waypoint "W_new" is added just before the first
-     * existing waypoint having routeIdx larger than w.position.
-     *
-     *
-     * Position defaults to Integer.MAX_VALUE so that newly created waypoints
-     * without other information are added to the end of the ridepoints list.
-     *
-     */
-    private Integer position = new Integer(Integer.MAX_VALUE);
+			this.setLongitude(webflowPoint.getLon());
+			this.setLatitude(webflowPoint.getLat());
 
-    public Integer getPosition() {
-        return this.position;
-    }
+			this.setDescription(webflowPoint.getDisplaystring());
 
-    public void setPosition(Integer position) {
-        this.position = position;
-    }
+			// set position parameter and rideIDs
 
-    /**
-     * Encode this waypoint as a JSON *Array* compatible with the representation
-     * of {@link JRoutePointEntity} that is:
-     * [longitude,latitude,id,'description']
-     *
-     * @return representation of this object as a json array
-     *
-     */
-    public StringBuffer getJSON() {
-        StringBuffer buf = new StringBuffer();
-        buf.append("[ ");
-        buf.append(this.getLongitude());
-        buf.append(", ");
-        buf.append(this.getLongitude());
-        buf.append(", ");
-        buf.append(this.getId());
-        buf.append(", ");
-        buf.append("'");
-        buf.append(this.getDescription());
-        buf.append("'");
-        buf.append(", ");
-        buf.append(this.getRideId());
-        buf.append(", ");
-        buf.append(this.getPosition());
-        buf.append(", ");
-        buf.append(this.getRouteIdx());
-        buf.append(" ]");
-        return buf;
-    }
+		} catch (Exception exc) {
+			System.err.println(exc);
+		}
 
-    /**
-     * Create Waypoint Entity from it's superclass, copying
-     * longitude,latitude,rideId,routeIdx and description.
-     *
-     */
-    protected JWaypointEntity(WaypointEntity w) {
+	} // smartUpdate
 
-        super();
-        this.setLongitude(w.getLongitude());
-        this.setLatitude(w.getLatitude());
-        this.setRideId(w.getRideId());
-        this.setRouteIdx(w.getRouteIdx());
-        this.setDescription(w.getDescription());
-    }
+	/**
+	 * JWaypoint Entity adds a volatile position parameter to WaypointEntity,
+	 *
+	 * This is used *only* for finding the routeIdx of a newly created waypoint.
+	 *
+	 * I.e a newly created waypoint "W_new" is added just before the first
+	 * existing waypoint having routeIdx larger than w.position.
+	 *
+	 *
+	 * Position defaults to Integer.MAX_VALUE so that newly created waypoints
+	 * without other information are added to the end of the ridepoints list.
+	 *
+	 */
+	private Integer position = new Integer(Integer.MAX_VALUE);
 
-    /**
-     * Dumb default constructor
-     */
-    public JWaypointEntity() {
-        super();
-    }
+	public Integer getPosition() {
+		return this.position;
+	}
 
-    /**
-     * True, if location is set for this waypoint. This is "step one" of the
-     * waypoint initialisation
-     *
-     * @return true, if longitude,latitude and description are !=null each
-     */
-    public boolean getLocationSet() {
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
 
-        return (this.getLatitude() != null
-                && this.getLongitude() != null
-                && this.getDescription() != null);
+	/**
+	 * Encode this waypoint as a JSON *Array* compatible with the representation
+	 * of {@link JRoutePointEntity} that is:
+	 * [longitude,latitude,id,'description']
+	 *
+	 * @return representation of this object as a json array
+	 *
+	 */
+	public StringBuffer getJSON() {
+		StringBuffer buf = new StringBuffer();
+		buf.append("[ ");
+		buf.append(this.getLongitude());
+		buf.append(", ");
+		buf.append(this.getLongitude());
+		buf.append(", ");
+		buf.append(this.getId());
+		buf.append(", ");
+		buf.append("'");
+		buf.append(this.getDescription());
+		buf.append("'");
+		buf.append(", ");
+		buf.append(this.getRideId());
+		buf.append(", ");
+		buf.append(this.getPosition());
+		buf.append(", ");
+		buf.append(this.getRouteIdx());
+		buf.append(" ]");
+		return buf;
+	}
 
-    }
+	/**
+	 * Create Waypoint Entity from it's superclass, copying
+	 * longitude,latitude,rideId,routeIdx and description.
+	 *
+	 */
+	protected JWaypointEntity(WaypointEntity w) {
 
-    /**
-     * This is step 2 of waypoint initialisation.
-     *
-     * @return true, iff both rideId and routeIdx are !=null
-     */
-    public boolean getRideAttached() {
+		super();
+		this.setLongitude(w.getLongitude());
+		this.setLatitude(w.getLatitude());
+		this.setRideId(w.getRideId());
+		this.setRouteIdx(w.getRouteIdx());
+		this.setDescription(w.getDescription());
+	}
 
-        return (this.getRideId() != null)
-                && (this.getRouteIdx() != null);
-    }
+	/**
+	 * Dumb default constructor
+	 */
+	public JWaypointEntity() {
+		super();
+	}
 
-    /**
-     * returns true, iff the initialisation is ready for persisting.
-     *
-     *
-     * @return this.isRideIdAttached() && this.isLocationSet()
-     */
-    public boolean getReadyForPersisting() {
-        return (this.getRideAttached() && this.getLocationSet());
-    }
+	/**
+	 * True, if location is set for this waypoint. This is "step one" of the
+	 * waypoint initialisation
+	 *
+	 * @return true, if longitude,latitude and description are !=null each
+	 */
+	public boolean getLocationSet() {
 
-    /**
-     * Extract a plain WaypointEntity object from this object.
-     *
-     * This makes sense when persisting a waypoint, since the entity manager
-     * does not now JWaypointEntities.
-     *
-     *
-     */
-    public WaypointEntity extractWaypoint() {
+		return (this.getLatitude() != null && this.getLongitude() != null && this
+				.getDescription() != null);
 
-        WaypointEntity res = new WaypointEntity();
+	}
 
-        res.setDescription(this.getDescription());
-        res.setLatitude(this.getLatitude());
-        res.setLongitude(this.getLongitude());
-        res.setRideId(this.getRideId());
-        res.setRouteIdx(this.getRouteIdx());
+	/**
+	 * This is step 2 of waypoint initialisation.
+	 *
+	 * @return true, iff both rideId and routeIdx are !=null
+	 */
+	public boolean getRideAttached() {
 
-        return res;
+		return (this.getRideId() != null) && (this.getRouteIdx() != null);
+	}
 
-    }
+	/**
+	 * returns true, iff the initialisation is ready for persisting.
+	 *
+	 *
+	 * @return this.isRideIdAttached() && this.isLocationSet()
+	 */
+	public boolean getReadyForPersisting() {
+		return (this.getRideAttached() && this.getLocationSet());
+	}
 
-    /**
-     * Call driverUndertakesRideService to add this waypoint to it's drive
-     */
-    public void addToDrive() {
+	/**
+	 * Extract a plain WaypointEntity object from this object.
+	 *
+	 * This makes sense when persisting a waypoint, since the entity manager
+	 * does not now JWaypointEntities.
+	 *
+	 *
+	 */
+	public WaypointEntity extractWaypoint() {
 
-        new JDriverUndertakesRideEntityService().addWaypointToDriveSafely(this);
-    }
+		WaypointEntity res = new WaypointEntity();
 
-    /**
-     * returns a value for the position parameter to addWaypoint(...) so that a
-     * point added with this parameter is placed immediately *before* this
-     * waypoint;
-     *
-     * @return this.getRouteIdx-0.5
-     */
-    public int getPositionValueBefore() {
-        return this.getRouteIdx();
-    }
+		res.setDescription(this.getDescription());
+		res.setLatitude(this.getLatitude());
+		res.setLongitude(this.getLongitude());
+		res.setRideId(this.getRideId());
+		res.setRouteIdx(this.getRouteIdx());
 
-    /**
-     * returns a value for the position parameter to addWaypoint(...) so that a
-     * point added with this parameter is placed immediately *after* this
-     * waypoint;
-     *
-     * @return this.getRouteIdx+0.5
-     */
-    public int getPositionValueAfter() {
-        return this.getRouteIdx()+1;
-    }
-    
-    
-    /** Remove this waypoint from the driverUndertakesRideEntity it is attached to
-     * 
-     */
-    public void removeFromDrive(){
-        new JDriverUndertakesRideEntityService().removeWaypointFromDriveSafely(this.getRideId().getRideId(), this.getRouteIdx());
-    }
-    
-    /** Position of waypoint to be displayed.
-     *  Since rideIdx property is 0-based, we use this to display a 1-based route index.
-     * 
-     * @return getRouteIdx+1
-     */
-    public int getDisplayRouteIndex(){
-        return this.getRouteIdx()+1;
-    }
+		return res;
 
-    /** Wayponts are naturally compared by comparing the route index.
-     *  Lower Ride Index comes firs.
-     * 
-     * @param o
-     * @return
-     */
+	}
+
+	/**
+	 * Call driverUndertakesRideService to add this waypoint to it's drive
+	 */
+	public void addToDrive() {
+
+		new JDriverUndertakesRideEntityService().addWaypointToDriveSafely(this);
+	}
+
+	/**
+	 * returns a value for the position parameter to addWaypoint(...) so that a
+	 * point added with this parameter is placed immediately *before* this
+	 * waypoint;
+	 *
+	 * @return this.getRouteIdx-0.5
+	 */
+	public int getPositionValueBefore() {
+		return this.getRouteIdx();
+	}
+
+	/**
+	 * returns a value for the position parameter to addWaypoint(...) so that a
+	 * point added with this parameter is placed immediately *after* this
+	 * waypoint;
+	 *
+	 * @return this.getRouteIdx+0.5
+	 */
+	public int getPositionValueAfter() {
+		return this.getRouteIdx() + 1;
+	}
+
+	/**
+	 * Remove this waypoint from the driverUndertakesRideEntity it is attached
+	 * to
+	 * 
+	 */
+	public void removeFromDrive() {
+		new JDriverUndertakesRideEntityService().removeWaypointFromDriveSafely(
+				this.getRideId().getRideId(), this.getRouteIdx());
+	}
+
+	/**
+	 * Position of waypoint to be displayed. Since rideIdx property is 0-based,
+	 * we use this to display a 1-based route index.
+	 * 
+	 * @return getRouteIdx+1
+	 */
+	public int getDisplayRouteIndex() {
+		return this.getRouteIdx() + 1;
+	}
+
+	/**
+	 * Wayponts are naturally compared by comparing the route index. Lower Ride
+	 * Index comes firs.
+	 * 
+	 * @param o
+	 * @return
+	 */
 	@Override
 	public int compareTo(JWaypointEntity o) {
-	
-		if(this.getRouteIdx()>o.getRouteIdx()){return 1;}
-		if(this.getRouteIdx()<o.getRouteIdx()){return -1;}
+
+		if (this.getRouteIdx() > o.getRouteIdx()) {
+			return 1;
+		}
+		if (this.getRouteIdx() < o.getRouteIdx()) {
+			return -1;
+		}
 		return 0;
 	}
 
-	
-	/** ordinary method to test wether given String is null or empty
-	 * 
-	 * @param arg   string to be tested
-	 * @return true, if argument is null or consists of whitespaces only
-	 */
-	private boolean isNullOrEmpty(String arg){
-		if(arg==null){return true;}
-		if("".equals(arg.trim())){ return true;}
-		return false;
-	}
-    
 }
