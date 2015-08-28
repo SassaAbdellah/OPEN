34fe1f17899ce0a2552fef6c9cb9c8a133665028
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-08-28 21:45:23 +0000

    Changes for alternative "Thessaloniki" frontend, and customizing see ticket #196. Better error handling. Blanking out previous error messages on every search, showing qualified error message if search does not get a resul.

--- trunk/src/OpenRideServer/joride/web/customization/thessaloniki/resources/js/callbackGoogleThessaloniki.js
+++ trunk/src/OpenRideServer/joride/web/customization/thessaloniki/resources/js/callbackGoogleThessaloniki.js
@@ -1,41 +1,47 @@
 
 
-/**
- * 
+/** Global variable containing the name of the "address" field to update.
+ *  This should typically be one of "f1:startAddress" or "f1:endAddress"
+ *  depending on whether start or destination is to be updated 
  */
 
 var addressField;
 
-/**
- * 
+/** Global variable containing the name of the "longitude" field to update.
+ *  This should typically be one of "f1:startLongitude" or "f1:endLongitude"
+ *  depending on whether start or destination is to be updated 
  */
 
 var longitudeField;
 
-/**
- * 
+/** Global variable containing the name of the "longitude" field to update.
+ *  This should typically be one of "f1:startLongitude" or "f1:endLongitude"
+ *  depending on whether start or destination is to be updated 
  */
+
 
 var latitudeField;
 
 
-/**
- * 
+/** Call google geocoder to update the startpoint.
+ *  Set global variables for fieldnames to be updated,
+ *  And then call codeAddressWithGoogle
  * 
  */
 function codeAddressWithGoogleStartPt() {
 		
-
 	addressField   ='f1:addressStart';
     longitudeField ='f1:longitudeStart';
 	latitudeField  ='f1:latitudeStart';
-	
+	errorField     ='divErrorStart';
+		
 	codeAddressWithGoogleSR();
 }
 
 
-/**
- * 
+/** Call google geocoder to update the endpoint.
+ *  Set global variables for fieldnames to be updated,
+ *  And then call codeAddressWithGoogle
  * 
  */
 function codeAddressWithGoogleEndPt() {
@@ -43,6 +49,7 @@ function codeAddressWithGoogleEndPt() {
 	addressField   ='f1:addressEnd';
     longitudeField ='f1:longitudeEnd';
 	latitudeField  ='f1:latitudeEnd';
+	 errorField     ='divErrorEnd';
 	
 	codeAddressWithGoogleSR();
 }
@@ -50,13 +57,16 @@ function codeAddressWithGoogleEndPt() {
 
 
 
-/**
- * 
- * 
- * 
+/** Get input from field named "addressField", 
+ *  Then call google geomapper to find places for which 
+ *  the input of address field matches.
+ *  If results are returned, update fields address/longitude/latitude
+ *  with the respective data.
+ *  
  * 
  */	
 function codeAddressWithGoogleSR() {
+	
 	
 	
   	var address = document.getElementById(addressField).value;
@@ -64,25 +74,28 @@ function codeAddressWithGoogleSR() {
     
 	function handleGMResult(results, status) {
 		
-					// blank out longitude and latitude not to have wrong values if we had no results
+					// remove all previous error messages
+					$('.formError').html('');
+		
+					// blank out longitude and latitude  not to have wrong values if we had no results
 					document.getElementById(longitudeField).value='';
 					document.getElementById(latitudeField).value='';
-					
-					
-    				if (status == google.maps.GeocoderStatus.OK ) {	
-    					
-					   if(bounds.contains( results[0].geometry.location)){
+				
 						
+    				if ( 
+    						(status == google.maps.GeocoderStatus.OK ) 	
+    						&&
+    						(bounds.contains( results[0].geometry.location))
+    				 ){
 					 	document.getElementById(addressField).value=(results[0].formatted_address);
                       	document.getElementById(longitudeField).value=(results[0].geometry.location.lng());
     					document.getElementById(latitudeField).value=(results[0].geometry.location.lat());
+    					
                				
-						}              
-     		 		        } else {
-     		 		     
-
-      						// console.log('Geocode was not successful for the following reason: ' + status);
-    					}
+    				} else {
+    					// set single errormessage for failed search
+     		 		    $("#"+errorField).html(noResults);
+    				}
     				
 			} // function handleGMResult
 	
