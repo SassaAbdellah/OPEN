70205b554c7651f78b7984a39a9c8b21118355e8
jochen <jochen@77871e9a-9e4b-4c3b-b6ca-f4d8c66ed517>
2015-08-29 13:51:43 +0000

    Adding on the fly validation for startTimeEarliest/StartTimeLatest, see ticket #198

---
+++ trunk/src/OpenRideServer/joride/web/resources/js/rideCreateFunctions.js
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * javascript functions used on ride create page
+ * *****************************************************************************
+ * 
+ */
+
+/** Function to be called when "starttime earliest" time picker is called
+ *  Just takes care that "startTimeLatest" is not before starttime earliest
+ */
+function onStartimeEarliestChange(){
+	
+	adjustLatestToEarliest();
+}
+
+
+/** Function to be called when "starttime latest" time picker is called
+ * 
+ */
+function onStarttimeLatestChange(){
+	adjustLatestToEarliest();
+}
+
+
+/** Set starttimeLatest to maximum of starttimeEarliest, starttimeLatest
+ *  Just takes care that "startTimeLatest" is not before starttime earliest
+ * 
+ */
+function adjustLatestToEarliest(){
+
+	//get hold of the two calendars
+	calEarliest=PF('starttimeEarliestCal');
+	calLatest=PF('starttimeLatestCal');
+
+	// if calendar for latest starttime is before earliestStarttime, then fix this
+	earliestDate=new Date(calEarliest.getDate());
+	latestDate=new Date(calLatest.getDate());
+	// adjust 
+	if(earliestDate.getTime()>latestDate.getTime()){
+	calLatest.setDate(earliestDate);
+	}
+}
+	
+	
+	
+	
